8216dbe8c7926e71fea56c381434ffc8e64c56a6
Pierre du Plessis <pierre@pcservice.co.za>
2017-12-04 09:24:45 +0200

    Add sample code for day 4 part 2

--- src/day4/test.php
+++ src/day4/test.php
@@ -9,5 +9,11 @@ return [
     ],
 
     // part 2
-    []
+    [
+        'abcde fghij' => 1,
+        'abcde xyz ecdab' => 0,
+        'a ab abc abd abf abj' => 1,
+        'iiii oiii ooii oooi oooo' => 1,
+        'oiii ioii iioi iiio' => 0,
+    ]
 ];
